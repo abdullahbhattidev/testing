4fee7876d774456d652609edad7e0be22bc40fbc
abdullahbhattidev <ab76293@gmail.com>
2022-10-10 18:43:55 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
+)
